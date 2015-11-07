6c9e9851826228ea3876af64b6f0d4852bcc8102
RafalJaworski <admin@lux-price.pl>
2015-11-07 17:08:52 +0100

    make service

--- app/src/main/java/domenafirmy/fakedownloadservice/activities/fakeDownloadService.java
+++ app/src/main/java/domenafirmy/fakedownloadservice/activities/fakeDownloadService.java
@@ -1,7 +1,19 @@
 package domenafirmy.fakedownloadservice.activities;
 
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
 /**
  * Created by jawa on 07/11/2015.
  */
-public class fakeDownloadService {
+public class fakeDownloadService extends Service{
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
