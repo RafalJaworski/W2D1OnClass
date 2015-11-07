11d52ec05581dcdabe54754184428818b08b8bcc
RafalJaworski <admin@lux-price.pl>
2015-11-07 17:38:15 +0100

    bind butterknife

--- app/src/main/java/domenafirmy/fakedownloadservice/activities/MainActivity.java
+++ app/src/main/java/domenafirmy/fakedownloadservice/activities/MainActivity.java
@@ -3,6 +3,7 @@ package domenafirmy.fakedownloadservice.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
 import domenafirmy.fakedownloadservice.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
